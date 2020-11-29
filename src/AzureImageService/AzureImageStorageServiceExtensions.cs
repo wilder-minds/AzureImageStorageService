f25bdd756534d11edfdd8be90cfffdaf62b40097
Shawn Wildermuth <shawn@wildermuth.com>
2020-11-29 16:29:31 -0500

    Update Docs for DI static

--- src/AzureImageService/AzureImageStorageServiceExtensions.cs
+++ src/AzureImageService/AzureImageStorageServiceExtensions.cs
@@ -6,6 +6,15 @@ namespace Microsoft.Extensions.DependencyInjection
 {
   public static class AzureImageStorageServiceExtensions
   {
+    /// <summary>
+    /// Adds the azure image storage service to the dependency injection layer.
+    /// </summary>
+    /// <param name="coll">The coll.</param>
+    /// <param name="azureAccountName">Name of the azure account.</param>
+    /// <param name="azureAccountKey">The azure account key (API Key).</param>
+    /// <param name="azureStorageUrl">The azure storage URL.</param>
+    /// <returns></returns>
+    /// <exception cref="InvalidOperationException">Azure Account Key must be a Base64 Encoded String. If running in development, mock the IImageStorageService for development instead.</exception>
     public static IServiceCollection AddAzureImageStorageService(this IServiceCollection coll, 
       [NotNull] string azureAccountName,
       [NotNull] string azureAccountKey,
